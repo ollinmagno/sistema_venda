ee51f7e6f9311dbed6cf8fc984eac96ec0573eed
unknown <41454237+ollinmagno@users.noreply.github.com>
2021-08-11 19:27:53 -0300

    método de listar produtos desativados adicionado

--- src/br/com/sistema/dao/ProdutoDAO.java
+++ src/br/com/sistema/dao/ProdutoDAO.java
@@ -13,7 +13,7 @@ public class ProdutoDAO {
 	
 
 	public void adicionaProduto(Produto produto) throws SQLException {
-		String sql = "INSERT INTO PRODUTO (NOME, DESCRICAO, PRECO) VALUES (?,?,?);";
+		String sql = "INSERT INTO PRODUTO (NOME, DESCRICAO, PRECO, DESATIVADO) VALUES (?,?,?,0);";
 		try (Connection connection = new ConnectionFactory().recuperarConexao()) {
 			try (PreparedStatement pstm = connection.prepareStatement(sql)) {
 				pstm.setString(1, produto.getNome());
@@ -29,7 +29,7 @@ public class ProdutoDAO {
 	public List<Produto> listaProdutos() throws SQLException{
 		List<Produto> listaProdutos = new ArrayList<Produto>();
 		
-		String sql = "SELECT ID, NOME, DESCRICAO, PRECO FROM PRODUTO;";
+		String sql = "SELECT ID, NOME, DESCRICAO, PRECO, DESATIVADO FROM PRODUTO;";
 		try(Connection connection = new ConnectionFactory().recuperarConexao()){
 			try(PreparedStatement pstm = connection.prepareStatement(sql)){
 				try(ResultSet resultset = pstm.executeQuery()){
@@ -39,11 +39,34 @@ public class ProdutoDAO {
 						produto.setNome(resultset.getString(2));
 						produto.setDescricao(resultset.getString(3));
 						produto.setPreco(resultset.getBigDecimal(4));
+						produto.setDesativado(resultset.getBoolean(5));
 						listaProdutos.add(produto);
 					}
 				}
 			}
 		}
 		return listaProdutos;
+	}
+	
+	public List<Produto> listaProdutosDesativadosDoSistema() throws SQLException {
+		List<Produto> listaProdutosDesativados = new ArrayList<Produto>();
+		
+		String sql = "SELECT ID, NOME, DESCRICAO, PRECO, DESATIVADO FROM PRODUTO WHERE DESATIVADO = 1;";
+		try(Connection connection = new ConnectionFactory().recuperarConexao()){
+			try(PreparedStatement pstm = connection.prepareStatement(sql)){
+				try(ResultSet resultset = pstm.executeQuery()){
+					while(resultset.next()) {
+						Produto produto = new Produto();
+						produto.setId(resultset.getInt(1));
+						produto.setNome(resultset.getString(2));
+						produto.setDescricao(resultset.getString(3));
+						produto.setPreco(resultset.getBigDecimal(4));
+						produto.setDesativado(resultset.getBoolean(5));
+						listaProdutosDesativados.add(produto);
+					}
+				}
+			}
+		}
+		return listaProdutosDesativados;
 	}
 }
